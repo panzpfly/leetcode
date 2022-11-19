1f197ae60fbe0c99534a5bda502c89372ebb8839
panzpfly <787998462@qq.com>
2022-11-19 20:16:00 +0800

    206.反转链表

---
+++ 206.反转链表.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* pre = NULL;
+        while(cur != NULL){
+            ListNode* temp = cur -> next;
+            cur -> next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+};
+// @lc code=end
+
