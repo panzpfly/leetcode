9f7f54a0c4601ef90e4ce9b2286bafcc4fab5432
panzpfly <787998462@qq.com>
2022-11-21 10:35:35 +0800

    24.两两交换链表中的节点

--- 24.两两交换链表中的节点.cpp
+++ 24.两两交换链表中的节点.cpp
@@ -18,7 +18,18 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-
+        ListNode* dummyhead = new ListNode(0);
+        dummyhead -> next = head;
+        ListNode* cur = dummyhead;
+        while(cur -> next != NULL&&cur -> next -> next != NULL){
+        ListNode* temp1 = cur -> next;
+        ListNode* temp2 = cur -> next -> next -> next;
+        cur -> next = cur -> next -> next;
+        cur -> next -> next = temp1;
+        temp1 -> next = temp2;
+        cur = cur -> next -> next;
+        }
+        return dummyhead -> next;
     }
 };
 // @lc code=end
