47e7a3f7b91fd8bc99ba0cd0c8d667d8c430a100
panzpfly <787998462@qq.com>
2022-11-23 22:22:33 +0800

    160.相交链表

---
+++ 160.相交链表.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=cpp
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+//         ListNode* dummyhead1 = new ListNode(0);
+//         ListNode* dummyhead2 = new ListNode(0);
+//         dummyhead1 -> next = headA;
+//         dummyhead2 -> next = headB;
+//         while(dummyhead1 -> next != NULL){
+//             ListNode* cur = dummyhead2;
+//             while(cur -> next != NULL){
+//                 if(cur -> next == dummyhead1 -> next){
+//                     return cur -> next;
+//                 }
+//                 cur = cur -> next;
+//             }
+//             dummyhead1 = dummyhead1 -> next;
+//         }
+//         return NULL;
+
+//     }
+// };
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        int lenA = 0, lenB = 0;
+        while (curA != NULL) { // 求链表A的长度
+            lenA++;
+            curA = curA->next;
+        }
+        while (curB != NULL) { // 求链表B的长度
+            lenB++;
+            curB = curB->next;
+        }
+        curA = headA;
+        curB = headB;
+        // 让curA为最长链表的头，lenA为其长度
+        if (lenB > lenA) {
+            swap (lenA, lenB);
+            swap (curA, curB);
+        }
+        // 求长度差
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap--) {
+            curA = curA->next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != NULL) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA->next;
+            curB = curB->next;
+        }
+        return NULL;
+    }
+};
+// @lc code=end
+
