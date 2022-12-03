1e116d9dd895243b973780ad9195af3a38ce540e
panzpfly <787998462@qq.com>
2022-12-03 18:33:23 +0800

    141.环形链表

---
+++ 141.环形链表.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast -> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
