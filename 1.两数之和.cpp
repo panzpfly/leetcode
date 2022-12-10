ea0316f48557f2d821863af03a8a9b542deebd27
panzpfly <787998462@qq.com>
2022-12-10 18:31:03 +0800

    哈希表:1.两数之和

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -5,16 +5,33 @@
  */
 
 // @lc code=start
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = i + 1; j < n; ++j) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        std::unordered_map<int,int> map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            /* code */
+            auto iter = map.find(target - nums[i]);
+            if(iter != map.end()) {
+                return {iter->second, i};
             }
+            // 如果没找到匹配对,就把访问过的元素和下表加入到map中
+            map.insert(pair<int, int>(nums[i], i));
         }
         return {};
     }
